38e5edb103edd7e333eaa395e3cfc55ac981a992
Suriya <suriyame2107@gmail.com>
2025-02-23 16:02:18 +0530

    Learning...

---
+++ 54.2sum.js
@@ -0,0 +1,37 @@
+var twoSum = function(nums, target) {
+    // My try Its my method
+    // firstVal = Infinity;
+    // secondVal = Infinity;
+    // for (let i = 0; i < nums.length; i++) {
+    //     const firstVal = secondVal;
+    //     secondVal = nums[i];
+    //     let targetVal = firstVal + secondVal;
+    //     if (targetVal === target) {
+    //         let firstIndex = nums.indexOf(firstVal);
+    //         let secondIndex = nums.indexOf(secondVal);
+    //         return {
+    //             firstIndex,
+    //             secondIndex
+    //         }
+    //     }
+    // }
+    // Step 2 Brute force method
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         if (nums[i] + nums[j] === target) {
+    //             return [i, j]
+    //         }
+    //     }
+    // }
+    // step 3 Hash map
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+};
+console.log(twoSum([1, 2, 3, 4], 7));
