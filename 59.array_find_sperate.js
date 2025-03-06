8507f2ab5db7cb516dd700fbb38945c71ea4b77c
Suriya <suriyame2107@gmail.com>
2025-03-06 23:17:18 +0530

    chnages.

---
+++ 59.array_find_sperate.js
@@ -0,0 +1,13 @@
+function chunkArray(arr, size) {
+    let result = [];
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
+    }
+    return result;
+}
+
+// Example usage
+console.log(chunkArray([1, 2, 3, 4, 5], 1)); // [[1],[2],[3],[4],[5]]
+console.log(chunkArray([1, 9, 6, 3, 2], 3)); // [[1,9,6],[3,2]]
+console.log(chunkArray([8, 5, 3, 2, 6], 6)); // [[8,5,3,2,6]]
+console.log(chunkArray([], 1)); // []
