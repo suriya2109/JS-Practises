fdcb744c0fdb9b944b94546fed0b6bda2168ba04
Suriya <suriyame2107@gmail.com>
2025-03-13 22:33:19 +0530

    The practices...

---
+++ 65.compact-value.js
@@ -0,0 +1,26 @@
+function compactObject(obj) {
+    if (Array.isArray(obj)) {
+        return obj
+            .filter(Boolean)
+            .map(item => (Array.isArray(item) ? compactObject(item) : item))
+
+    } else if (obj && typeof obj === "object") {
+        let result = {};
+        for (let key in obj) {
+            if (Boolean(obj[key])) {
+                result[key] = compactObject(obj[key]);
+            }
+        }
+        return result;
+    }
+    return obj;
+};
+
+obj = [0, false, 1, "2", null];
+obj2 = { "a": null, "b": [false, 1] }
+obj3 = [null, 0, 5, [0],
+    [false, 16]
+]
+console.log(compactObject(obj));
+console.log(compactObject(obj2));
+console.log(compactObject(obj3));
