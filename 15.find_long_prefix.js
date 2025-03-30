73c02525518831736d369da432d521735ef41711
Suriya <suriyame2107@gmail.com>
2025-03-30 23:12:02 +0530

    The practices.

--- 15.find_long_prefix.js
+++ 15.find_long_prefix.js
@@ -6,6 +6,7 @@ function longestCommonPrefix(strs) {
     for (let i = 1; i < strs.length; i++) {
         while (strs[i].indexOf(prefix) !== 0) { // While prefix is not a prefix of strs[i]
             prefix = prefix.substring(0, prefix.length - 1); // Reduce prefix
+            // prefix = prefix.slice(0, -1);
             if (prefix === "") return ""; // If prefix becomes empty, return ""
         }
     }
