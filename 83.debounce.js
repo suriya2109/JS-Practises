dc8c9b1fcee07808e9d2d29cc1326b08c2662c34
Suriya <suriyame2107@gmail.com>
2025-04-14 18:09:12 +0530

    The practice.

---
+++ 83.debounce.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn(...args);
+        }, t)
+    }
+};
+
+let start = Date.now();
+
+function funcLog(...args) {
+    console.log(`${Date.now - start}ms:`, args);
+}
+
+const dlog = debounce(funcLog, 50);
+setTimeout(() => dlog(1), 50);
+setTimeout(() => dlog(2), 75);
