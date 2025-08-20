6bc017c90eb06cb8beb4e3c4bf609fc4d6ea89c2
Suriya <suriyame2107@gmail.com>
2025-08-20 23:39:07 +0530

    The practice.

--- 54.2sum.js
+++ 54.2sum.js
@@ -24,15 +24,30 @@ var twoSum = function(nums, target) {
     //     }
     // }
     // step 3 Hash map
-    let map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i];
-            // return [complement, nums[i]]; //values
+    // let map = new Map();
+    // for (let i = 0; i < nums.length; i++) {
+    //     let complement = target - nums[i];
+    //     if (map.has(complement)) {
+    //         return [map.get(complement), i];
+    //         // return [complement, nums[i]]; //values
+    //     }
+    //     map.set(nums[i], i);
+    // }
+    // return [];
+
+    left = 0;
+    right = nums.length - 1;
+    while (left < right) {
+        let sum = nums[left] + nums[right];
+        if (sum === target) {
+            return [nums[left], nums[right]];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
         }
-        map.set(nums[i], i);
     }
     return [];
+
 };
 console.log(twoSum([1, 2, 3, 4], 7));
