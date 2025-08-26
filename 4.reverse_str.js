02948e6679e63a4a7e9563d3bd95d627903bd28b
Suriya <suriyame2107@gmail.com>
2025-08-27 00:02:51 +0530

    The practice."

--- 4.reverse_str.js
+++ 4.reverse_str.js
@@ -1,38 +1,28 @@
-// function reverseFunc(str) {
-//     reversed = "";
-//     for (let i = str.length - 1; i >= 0; i--) {
-//         reversed += str[i];
-//     }
-//     return reversed;
-//     // let reverseString = str.split('').reverse().join('');
-//     // return reverseString;
-// }
-// console.log(reverseFunc('suriya'));
+function reverseFunc(str) {
+    // reversed = "";
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     reversed += str[i];
+    // }
+    // return reversed;
+    // let reverseString = str.split('').reverse().join('');
+    // return reverseString;
 
+    let i = 0;
+    let j = str.length - 1;
+
+    while (i < j) {
+        [str[i], str[j]] = [str[j], str[i]];
+        i++;
+        j--;
+    }
+    return str;
+}
+
+let s = ["h", "e", "l", "l", "o"];
+console.log(reverseFunc(s));
 // const str1 = "GeeksForGeeks";
 // const substrResult = str1.substr(2, 7); //It's take length.
 // const substringResult = str1.substring(2, 7); // It's start to end string.
 
 // console.log("Str.substr(7) =", substrResult);
 // console.log("Str.substring(7) =", substringResult);
-
-function twoSum(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-
-    while (left < right) {
-        let sum = nums[left] + nums[right];
-        if (sum === target) {
-            return [left, right];
-        } else if (sum < left) {
-            left++;
-        } else {
-            right--;
-        }
-    }
-    return [];
-};
-
-let nums = [2, 7, 11, 15];
-let target = 9;
-console.log(twoSum(nums, target));
