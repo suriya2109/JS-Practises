73100d27c1db9a35d0250cd81ac72e313d810fb7
Suriya <suriyame2107@gmail.com>
2025-09-07 23:43:47 +0530

    The practices.

---
+++ sorting/1.largestNumer.js
@@ -0,0 +1,15 @@
+function largestNumber(nums) {
+    let num = nums.map(String);
+    // let result = num.sort((a, b) => (b + a) - (a + b));
+    let result = num.sort((a, b) => (b + a).localeCompare(a + b));
+
+    if (result[0] === '0') {
+        console.log('0');
+        return;
+    }
+    console.log(result.join(''));
+
+}
+
+// largestNumber([10, 2]); // "210"
+largestNumber([3, 30, 34, 5, 9]); // "9534330"
