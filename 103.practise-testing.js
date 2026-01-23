1bd965d9165cda814f3f4b503e3b64668895a646
Suriya <suriyacosmo@gmail.com>
2026-01-23 19:04:46 +0530

    The practice of eloquent js.

--- 103.practise-testing.js
+++ 103.practise-testing.js
@@ -40,18 +40,78 @@
 // console.log(fizzBuzz(nums));
 
 
-function chessBoard(num) {
-    for (let i = 0; i < num; i++) {
-        let str = " ";
-        for (let j = 0; j < num; j++) {
-            if ((i + j) % 2 === 0) {
-                str += " ";
-            } else {
-                str += "#";
-            }
+// function chessBoard(num) {
+//     for (let i = 0; i < num; i++) {
+//         let str = " ";
+//         for (let j = 0; j < num; j++) {
+//             if ((i + j) % 2 === 0) {
+//                 str += " ";
+//             } else {
+//                 str += "#";
+//             }
+//         }
+//         console.log(str);
+//     }
+// }
+
+// chessBoard(8);
+
+// function zeroPad(number, width) {
+//     let string = String(number);
+//     while (string.length < width) {
+//         string = "0" + string;
+//     }
+//     return string;
+// }
+
+// function printFarmInventory(cows, chickens, pigs) {
+//     console.log(`${zeroPad(cows, 3)} Cows`);
+//     console.log(`${zeroPad(chickens, 3)} Chickens`);
+//     console.log(`${zeroPad(pigs, 3)} Pigs`);
+// }
+// printFarmInventory(7, 16, 3);
+
+// function mathMin(s, l) {
+//     if (s < l) {
+//         return s;
+//     } else {
+//         return l;
+//     }
+// }
+
+// console.log(mathMin(6, 6))
+
+// function oddEven(num) {
+//     if (num % 2 == 0) {
+//         console.log("even");
+//     } else {
+//         console.log("odd");
+//     }
+// }
+
+// oddEven(1200000);
+
+// function isEven(n) {
+//     if (n === 0) return true;
+//     if (n === 1) return false;
+//     return isEven(n - 2);
+// }
+
+// console.log(isEven(50));
+// console.log(isEven(75));
+
+function countChar(string, n) {
+    let countBs = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === n) {
+            countBs++;
         }
-        console.log(str);
     }
+    return countBs;
 }
 
-chessBoard(8);
+function countBean(string) {
+    return countChar(string, "o");
+}
+
+console.log(countBean("the Ball is red colour"))
