cc764a927c136ff55c40a02adada9d09977e38f2
Suriya <suriyacosmo@gmail.com>
2026-05-15 22:59:33 +0530

    Test.

--- 1.08.js
+++ 1.08.js
@@ -1,43 +1,66 @@
-let mp = [{
-        name: "abc",
-        firstName: "abcJ"
-    },
-    {
-        name: "def",
-        firstName: "defJ"
-    },
-    {
-        language: "ghi",
-        firstName: "tamil"
-    },
-    {
-        language: "ghi",
-        firstName: "english"
-    },
-    {
-        language: "ghi",
-        firstName: "french"
-    }
-];
+// let mp = [{
+//         name: "abc",
+//         firstName: "abcJ"
+//     },
+//     {
+//         name: "def",
+//         firstName: "defJ"
+//     },
+//     {
+//         language: "ghi",
+//         firstName: "tamil"
+//     },
+//     {
+//         language: "ghi",
+//         firstName: "english"
+//     },
+//     {
+//         language: "ghi",
+//         firstName: "french"
+//     }
+// ];
 
-let res = mp.reduce((acc, item) => {
+// let res = mp.reduce((acc, item) => {
 
-    // For name property
-    if (item.name) {
-        acc[item.name] = item.firstName;
-    }
+//     // For name property
+//     if (item.name) {
+//         acc[item.name] = item.firstName;
+//     }
 
-    // For language property
-    if (item.language) {
-        if (!acc[item.language]) {
-            acc[item.language] = [];
+//     // For language property
+//     if (item.language) {
+//         if (!acc[item.language]) {
+//             acc[item.language] = [];
+//         }
+
+//         acc[item.language].push(item.firstName);
+//     }
+
+//     return acc;
+
+// }, {});
+
+// console.log(res);
+
+
+let str = "hElLO worLd";
+
+function toUpperCase(str) {
+    let obj = {};
+
+    str.split("").forEach(char => {
+        if (char === char.toUpperCase() && char !== " ") {
+            obj[char] = (obj[char] || 0) + 1;
         }
+    });
 
-        acc[item.language].push(item.firstName);
+    // Convert count into E_1, L_3 format
+    for (let key in obj) {
+        if (obj[key] > 1) {
+            return `${key}_${obj[key]}`;
+        }
     }
 
-    return acc;
+}
 
-}, {});
-
-console.log(res);
+console.log(toUpperCase(str));
